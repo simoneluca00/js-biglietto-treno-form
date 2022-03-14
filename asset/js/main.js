3e54401187d4b9c8f5704f611983511afeaffc4d
simoneluca00 <si.zaino@libero.it>
2022-03-14 17:39:07 +0100

    Esercizio terminato (corr. funzione js accorgim.)

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,50 +1,54 @@
-// 1) RECUPERARE IL VALUE INSERITO NEGLI INPUT
-
-    // 1.1)nome del passeggero
-let nomePasseggero = document.getElementById("sz_input_name");
-
-    // 1.2)km da fare 
-let km = Number(document.getElementById("sz_input_km").value);
-console.log(typeof km);
-
-
-        // 1.2.1) MOLTIPLICAZIONE PER IL CALCOLO DEL PREZZO BASATO SUI km
-let tassa = 0.21;
-
-let prezzoStandard = tassa * km;
-prezzoStandard = Number(prezzoStandard.toFixed(2));
-console.log(typeof prezzoStandard);
-
-    // 1.3)età del passeggero
-let etaPasseggero = document.getElementById("age-range");
-
-        // 1.3.1) CALCOLO DEL PREZZO PER I MINORENNI (sconto del 20%) 
-let prezzoMinorenni = (prezzoStandard * 0.8);
-prezzoMinorenni = Number(prezzoMinorenni.toFixed(2));
-
-        // 1.3.2) CALCOLO DEL PREZZO PER GLI OVER 65 (sconto del 40%)
-let prezzoAnziani = (prezzoStandard * 0.6);
-prezzoAnziani = Number(prezzoAnziani.toFixed(2));
-
-    // 1.4)definire i bottoni 
+    // 1)definire i bottoni 
 let buttonRun = document.getElementById("sz_btn_run");
 let buttonCancel = document.getElementById("sz_btn_cancel");
 
-    //1.5) definire campo del prezzo finale
-let prezzoFinale =document.getElementById("sz_price_pass");
+    // 2) definire campo del prezzo finale
+let prezzoFinale = document.getElementById("sz_price_pass");
 
-    // 1.6)funzione al click del bottone "Esegui"
+    // 3)funzione al click del bottone "Esegui"
 buttonRun.addEventListener("click",
     function () {
-        // far comparire la sezione con tutte le indicazioni del biglietto
+        // 3.1) far comparire la sezione con tutte le indicazioni del biglietto
         var ticketCompleto = document.getElementById("sz_ticket");
         ticketCompleto.classList.remove("d-none");
-        
-        // nome e prezzo del biglietto stampati sulla pagina
+
+        // 4)nome del passeggero
+        let nomePasseggero = document.getElementById("sz_input_name");
+
+        // 5)km da fare 
+        let km = Number(document.getElementById("sz_input_km").value);
+        console.log(typeof km);
+
+            // 5.1) MOLTIPLICAZIONE PER IL CALCOLO DEL PREZZO BASATO SUI km
+        let tassa = 0.21;
+
+        let prezzoStandard = tassa * km;
+        prezzoStandard = Number(prezzoStandard.toFixed(2));
+        console.log(typeof prezzoStandard);
+
+        // 6)età del passeggero
+        let etaPasseggero = document.getElementById("age-range");
+
+            // 6.1) CALCOLO DEL PREZZO PER I MINORENNI (sconto del 20%) 
+        let prezzoMinorenni = (prezzoStandard * 0.8);
+        prezzoMinorenni = Number(prezzoMinorenni.toFixed(2));
+
+            // 6.2) CALCOLO DEL PREZZO PER GLI OVER 65 (sconto del 40%)
+        let prezzoAnziani = (prezzoStandard * 0.6);
+        prezzoAnziani = Number(prezzoAnziani.toFixed(2));
+
+        // 7) nome e prezzo del biglietto stampati sulla pagina
         document.getElementById("sz_name_pass").innerHTML = `${nomePasseggero.value}`;
         document.getElementById("sz_discount_pass").innerHTML = `${etaPasseggero.value}`;
         
-        // condizione per il calcolo del prezzo
+        // 8) condizione per eventuali campi vuoti
+        if (nomePasseggero.value == "") {
+            alert ("Inserire correttamente il nominativo per calcolare il prezzo.")
+        } else if (km == 0) {
+            alert ("Assicurati che il numero di km che vuoi percorrere sia maggiore di 0.")
+        }
+
+        // 9) condizione per il calcolo del prezzo
         if ( etaPasseggero.value == "Minorenne" ) {
            prezzoFinale.innerHTML = `${prezzoMinorenni}€ (IVA Compresa).`;
         }
@@ -56,13 +60,13 @@ buttonRun.addEventListener("click",
         else {
            prezzoFinale.innerHTML = `${prezzoStandard}€ (IVA Compresa).`;
         }
-        
-        // generatore di numeri casuali (carrozza)
+
+        // 10) generatore di numeri casuali (carrozza)
         var numeroCarrozza = Math.random();
         numeroCarrozza = Math.floor((numeroCarrozza * 15) + 1); 
         document.getElementById("sz_carriage_pass").innerHTML = `${numeroCarrozza}`;
         
-        // generatore di numeri casuali( Codice CP)
+        // 11) generatore di numeri casuali( Codice CP)
         var codiceCp = Math.random();
         codiceCp = Math.floor((codiceCp * 100000) + 10001); 
         document.getElementById("sz_pin_pass").innerHTML = `${codiceCp}`;
@@ -71,7 +75,7 @@ buttonRun.addEventListener("click",
     
 )
     
-        // 1.7)funzione al click del bottone "Annulla"
+        // 12)funzione al click del bottone "Annulla"
 buttonCancel.addEventListener("click",
     function() {
             // far scomparire la sezione con tutte le indicazioni del biglietto
